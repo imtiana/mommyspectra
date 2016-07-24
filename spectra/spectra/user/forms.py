c946708edb6b611cdd399b295969558b052e2a03
ayatsai <ctsai1011@gmail.com>
2016-07-23 19:21:42 -0700

    fix register to add userprofile

--- spectra/spectra/user/forms.py
+++ spectra/spectra/user/forms.py
@@ -1,9 +1,18 @@
 from django import forms
 from django.contrib.auth.models import User
 
+from spectra.user.models import UserProfile
+
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    phone_number = forms.CharField()
+
+    class Meta:
+        model = UserProfile
+        fields = ('phone_number', 'biography')
--- spectra/spectra/user/views.py
+++ spectra/spectra/user/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import redirect, render, render_to_response
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 from django import forms
 from django.template import RequestContext
 
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 
-from spectra.user.forms import UserForm
+from spectra.user.forms import UserForm, UserProfileForm
 from spectra.milk.models import *
 
 # Create your views here.
@@ -16,91 +17,59 @@ def user_login(request):
     # Like before, obtain the context for the user's request.
     context = RequestContext(request)
 
-    # If the request is a HTTP POST, try to pull out the relevant information.
     if request.method == 'POST':
-        # Gather the username and password provided by the user.
-        # This information is obtained from the login form.
         username = request.POST['username']
         password = request.POST['password']
 
-        # Use Django's machinery to attempt to see if the username/password
-        # combination is valid - a User object is returned if it is.
         user = authenticate(username=username, password=password)
 
-        # If we have a User object, the details are correct.
-        # If None (Python's way of representing the absence of a value), no user
-        # with matching credentials was found.
         if user:
-            # Is the account active? It could have been disabled.
             if user.is_active:
                 # If the account is valid and active, we can log the user in.
                 # We'll send the user back to the homepage.
                 login(request, user)
                 return HttpResponseRedirect('/milk/')
             else:
-                # An inactive account was used - no logging in!
                 return HttpResponse("Your Rango account is disabled.")
         else:
-            # Bad login details were provided. So we can't log the user in.
             print "Invalid login details: {0}, {1}".format(username, password)
             return HttpResponse("Invalid login details supplied.")
 
-    # The request is not a HTTP POST, so display the login form.
-    # This scenario would most likely be a HTTP GET.
     else:
-        # No context variables to pass to the template system, hence the
-        # blank dictionary object...
         return render_to_response('login.html', {}, context)
 
-# Use the login_required() decorator to ensure only those logged in can access the view.
 @login_required
 def user_logout(request):
-    # Since we know the user is logged in, we can now just log them out.
     logout(request)
-
-    # Take the user back to the homepage.
     return HttpResponseRedirect('/')
 
 def register(request):
     context = RequestContext(request)
 
-    # A boolean value for telling the template whether the registration was successful.
-    # Set to False initially. Code changes value to True when registration succeeds.
-    registered = False
-
-    # If it's a HTTP POST, we're interested in processing form data.
     if request.method == 'POST':
-        # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm.
         user_form = UserForm(data=request.POST)
+        up_form = UserProfileForm(data=request.POST)
 
-        # If the two forms are valid...
-        if user_form.is_valid():
-            # Save the user's form data to the database.
+        if user_form.is_valid() and up_form.is_valid():
             user = user_form.save()
-
-            # Now we hash the password with the set_password method.
-            # Once hashed, we can update the user object.
             user.set_password(user.password)
             user.save()
 
+            up = up_form.save(commit=False)
+            up.user = user
+            up.save()
+
+            user = authenticate(username=user.username, password=user_form.cleaned_data['password'])
+            login(request, user)
             return HttpResponseRedirect('/milk/')
 
-        # Invalid form or forms - mistakes or something else?
-        # Print problems to the terminal.
-        # They'll also be shown to the user.
-        else:
-            print user_form.errors
-
-    # Not a HTTP POST, so we render our form using two ModelForm instances.
-    # These forms will be blank, ready for user input.
     else:
         user_form = UserForm()
+        up_form = UserProfileForm()
 
-    # Render the template depending on the context.
     return render_to_response(
             'register.html',
-            {'user_form': user_form, 'registered': registered},
+            {'user_form': user_form, 'up_form': up_form},
             context)
 
 def profile(request):
